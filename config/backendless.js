6e9864f86b1e2f027702ab5aba9f0c0dcca5551c
Podvalnyi Mikhail <qwertypomy@gmail.com>
2019-04-07 12:15:05 +0300

    Add .eslintrc; add and configure backendless

---
+++ config/backendless.js
@@ -0,0 +1,12 @@
+import { Platform } from 'react-native';
+import Backendless from 'backendless';
+import 'backendless-react-native';
+
+const APP_ID = 'B17B73CF-BF02-64F7-FFF1-F848B2D1A400';
+
+const APP_KEY = Platform.select({
+  ios: 'EF7D54F3-0D54-B262-FF50-980A1C0ADE00',
+  android: 'F5CF28E7-5B9B-A683-FFEA-D6DA8AABA700'
+});
+
+Backendless.initApp(APP_ID, APP_KEY);
